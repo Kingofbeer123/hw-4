9db2295dc7bd1b6ca9144e87bda241585bd23275
Kingofbeer123 <kiril.shevchenko123@gmail.com>
2022-04-10 17:48:31 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-let name = prompt ('Hello! What you name?');
-alert( `Hello, ${name}! How are you?`);
+for (let i = 0; i < 100; i++) {
+    let a = prompt("break or continue??")
+    if (a==='break')break;
+    if (a==='continue')continue;
+    else {
+    alert(i);
+    }
+  }
