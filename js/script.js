e3f030b3f1072e276d8e4b9047c9ec800849a03a
Kingofbeer123 <102953959+Kingofbeer123@users.noreply.github.com>
2022-04-10 18:02:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 for (let i = 0; i < 100; i++) {
-    let a = prompt("break or continue??")
+    let a = prompt("break or continue??");
     if (a==='break')break;
     if (a==='continue')continue;
     else {
